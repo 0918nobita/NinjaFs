640f2877142524f11f8aed47f063f5271b1b0c2b
Kodai Matsumoto <nobita.0918@gmail.com>
2022-07-31 19:16:07 +0900

    :recycle: Do refactor

--- testLib/Library.fs
+++ testLib/Library.fs
@@ -1,80 +1,172 @@
-﻿[<AutoOpen>]
+[<AutoOpen>]
 module NinjaFs.TestLib.Library
 
-open FSharp.Quotations
 open FSharp.Quotations.Patterns
 
-let private (|NewAnonRecord|_|) (expr: Expr) : option<Map<string, Expr>> =
-    match expr with
-    | NewRecord (ty, fields) when ty.Name.StartsWith "<>f__AnonymousType" ->
-        ty.GetProperties()
-        |> Array.indexed
-        |> Array.map (fun (i, property) -> (property.Name, fields[i]))
-        |> Map.ofArray
-        |> Some
-    | _ -> None
+[<RequireQualifiedAccess>]
+type Expr =
+    | FunctionCall of func: string * args: list<Expr>
+    | Lambda of var: string * body: Expr
+    | MethodCall of object: Expr * method: string * args: list<Expr>
+    | NewAnonRecord of Map<string, Expr>
+    | NewUnionCase of name: string * fields: list<Expr>
+    | StringLiteral of value: string
+    | UnitLiteral
+    | VarRef of name: string
 
-type Expr with
-    static member ToIExpr(target: Expr) : Ast.Expr.IExpr =
-        match target with
+open Thoth.Json.Net
+
+module Expr =
+    let isSimpleExpr =
+        function
+        | Expr.NewAnonRecord _
+        | Expr.StringLiteral _
+        | Expr.UnitLiteral
+        | Expr.VarRef _ -> true
+        | Expr.FunctionCall _
+        | Expr.Lambda _
+        | Expr.MethodCall _
+        | Expr.NewUnionCase _ -> false
+
+    let rec jsonEncode =
+        function
+        | Expr.FunctionCall (func, args) ->
+            Encode.object [ ("type", Encode.string "functionCall")
+                            ("func", Encode.string func)
+                            ("args", encodeList args) ]
+        | Expr.Lambda (var, body) ->
+            Encode.object [ ("type", Encode.string "lambda")
+                            ("var", Encode.string var)
+                            ("body", jsonEncode body) ]
+        | Expr.MethodCall (object, method, args) ->
+            Encode.object [ ("type", Encode.string "methodCall")
+                            ("object", jsonEncode object)
+                            ("method", Encode.string method)
+                            ("args", encodeList args) ]
+        | Expr.NewAnonRecord fields ->
+            fields
+            |> Map.map (fun _k v -> jsonEncode v)
+            |> Map.toList
+            |> Encode.object
+        | Expr.NewUnionCase (name, fields) ->
+            Encode.object [ ("type", Encode.string "newUnionCase")
+                            ("name", Encode.string name)
+                            ("fields", encodeList fields) ]
+        | Expr.StringLiteral value ->
+            Encode.object [ ("type", Encode.string "stringLiteral")
+                            ("value", Encode.string value) ]
+        | Expr.UnitLiteral -> Encode.object [ ("type", Encode.string "unitLiteral") ]
+        | Expr.VarRef name ->
+            Encode.object [ ("type", Encode.string "varRef")
+                            ("name", Encode.string name) ]
+
+    and private encodeList list =
+        list |> List.map jsonEncode |> Encode.list
+
+    let rec toSrc: Expr -> string =
+        function
+        | Expr.FunctionCall (func, args) ->
+            let args =
+                args
+                |> List.map toSrcWithParen
+                |> String.concat " "
+
+            $"%s{func} %s{args}"
+        | Expr.Lambda (var, body) ->
+            let body = toSrc body
+            $"fun %s{var} -> %s{body}"
+        | Expr.MethodCall (object, method, args) ->
+            let object = toSrcWithParen object
+
+            let args =
+                args
+                |> List.map toSrcWithParen
+                |> String.concat ", "
+
+            $"%s{object}.%s{method}(%s{args})"
+        | Expr.NewAnonRecord fields ->
+            let fields =
+                fields
+                |> Map.map (fun k v ->
+                    let v = toSrcWithParen v
+                    $"%s{k} = %s{v}")
+                |> Map.toList
+                |> List.map snd
+                |> String.concat "; "
+
+            $"{{| %s{fields} |}}"
+        | Expr.NewUnionCase (name, fields) ->
+            let fields =
+                if List.isEmpty fields then
+                    ""
+                else
+                    fields
+                    |> List.map toSrcWithParen
+                    |> String.concat ", "
+                    |> sprintf "(%s)"
+
+            $"%s{name}%s{fields}"
+        | Expr.StringLiteral value -> $"\"%s{value}\""
+        | Expr.UnitLiteral -> "()"
+        | Expr.VarRef name -> $"%s{name}"
+
+    and private toSrcWithParen (expr: Expr) =
+        let src = toSrc expr
+
+        if isSimpleExpr expr then
+            src
+        else
+            $"(%s{src})"
+
+    let private (|NewAnonRecord|_|) (expr: Quotations.Expr) : option<Map<string, Quotations.Expr>> =
+        match expr with
+        | NewRecord (ty, fields) when ty.Name.StartsWith "<>f__AnonymousType" ->
+            ty.GetProperties()
+            |> Array.indexed
+            |> Array.map (fun (i, property) -> (property.Name, fields[i]))
+            |> Map.ofArray
+            |> Some
+        | _ -> None
+
+    let rec fromQuotationsExpr (expr: Quotations.Expr) : Expr =
+        match expr with
         | Call (object, method, args) ->
             match object with
-            | Some (ValueWithName (_value, _ty, "builder@")) ->
-                let target = Expr.ToIExpr args[0]
-
-                match target with
-                | :? Ast.BuilderMethodCall.BuilderMethodCall as (Ast.BuilderMethodCall.BuilderMethodCall payload) ->
-                    builderMethodCall
-                        {| Target = payload.Target
-                           Chain =
-                            payload.Chain
-                            @ [ {| Method = method.Name
-                                   Args = args[1..] |> List.map Expr.ToIExpr |} ] |}
-                | _ ->
-                    builderMethodCall
-                        {| Target = target
-                           Chain =
-                            [ {| Method = method.Name
-                                 Args = args[1..] |> List.map Expr.ToIExpr |} ] |}
-
             | Some object ->
-                let object = Expr.ToIExpr object
-                let args = args |> List.map Expr.ToIExpr
-
-                methodCall
-                    {| Object = object
-                       Method = method.Name
-                       Args = args |}
+                let object = fromQuotationsExpr object
+                let args = args |> List.map fromQuotationsExpr
+                Expr.MethodCall(object, method.Name, args)
             | None ->
-                let args = args |> List.map Expr.ToIExpr
+                let args = args |> List.map fromQuotationsExpr
 
                 let func =
                     $"%s{method.DeclaringType.Namespace}.%s{method.DeclaringType.Name}.%s{method.Name}"
 
-                functionCall {| Func = func; Args = args |}
+                Expr.FunctionCall(func, args)
         | Lambda (var, body) ->
-            let body = Expr.ToIExpr body
-            lambda {| VarName = var.Name; Body = body |}
+            let body = fromQuotationsExpr body
+            Expr.Lambda(var.Name, body)
         | NewAnonRecord fields ->
             let fields =
                 fields
-                |> Map.map (fun _ field -> Expr.ToIExpr field)
+                |> Map.map (fun _ field -> fromQuotationsExpr field)
 
-            newAnonRecord fields
+            Expr.NewAnonRecord fields
         | NewUnionCase (unionCaseInfo, fields) ->
-            let fields = fields |> List.map Expr.ToIExpr
+            let fields = fields |> List.map fromQuotationsExpr
 
-            newUnionCase
-                {| Name = unionCaseInfo.Name
-                   Fields = fields |}
-        | Var var -> varRef var.Name
-        | Value (obj, ty) when obj = () && ty = typeof<unit> -> unitLiteral
-        | Value (obj, ty) when ty = typeof<string> -> stringLiteral (obj :?> string)
-        | _ -> failwith $"Unsupported expression: %A{target}"
+            Expr.NewUnionCase(unionCaseInfo.Name, fields)
+        | Var var -> Expr.VarRef var.Name
+        | Value (:? unit, ty) when ty = typeof<unit> -> Expr.UnitLiteral
+        | Value (:? string as v, ty) when ty = typeof<string> -> Expr.StringLiteral v
+        | ValueWithName (_value, _ty, name) -> Expr.VarRef name
+        | _ -> failwith $"Unsupported expression: %A{expr}"
 
-open Thoth.Json.Net
-
-let writeSnapshot (filename: string) (expr: Expr) =
-    let content = Encode.toString 2 ((Expr.ToIExpr expr).Encoder())
+let writeSnapshot (filename: string) (expr: Quotations.Expr) =
+    let content =
+        expr
+        |> Expr.fromQuotationsExpr
+        |> Expr.jsonEncode
+        |> Encode.toString 2
 
     System.IO.File.WriteAllText(filename, $"{content}\n")
