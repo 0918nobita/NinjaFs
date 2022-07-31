91052ab39df8117270dbd65a7f34ee8a35a5f8bc
Kodai Matsumoto <nobita.0918@gmail.com>
2022-07-31 19:16:07 +0900

    :recycle: Do refactor

--- testLib/ast/Expr.fs
+++
@@ -1,7 +0,0 @@
-[<AutoOpen>]
-module NinjaFs.TestLib.Ast.Expr
-
-type IExpr =
-    abstract member IsSimpleExpr: bool
-    abstract member Encoder: unit -> Thoth.Json.Net.JsonValue
-    abstract member ReconstructSourceCode: unit -> string
