ad627437419815685dae5bc2eb152d83fcbebbdb
Kodai Matsumoto <nobita.0918@gmail.com>
2022-08-01 15:10:41 +0900

    :sparkles: ネストしたビルダークラスのメソッド呼び出しを短縮表記するようにした

--- testLib/Library.fs
+++ testLib/Library.fs
@@ -6,6 +6,7 @@ open FSharp.Quotations.Patterns
 [<RequireQualifiedAccess>]
 type Expr =
     | Application of func: Expr * arg: Expr
+    | BuilderMethodCallChain of object: Expr * chain: list<{| Method: string; Args: list<Expr> |}>
     | FunctionCall of func: string * args: list<Expr>
     | Lambda of var: string * body: Expr
     | MethodCall of object: Expr * method: string * args: list<Expr>
@@ -26,6 +27,7 @@ module Expr =
         | Expr.UnitLiteral
         | Expr.VarRef _ -> true
         | Expr.Application _
+        | Expr.BuilderMethodCallChain _
         | Expr.FunctionCall _
         | Expr.Lambda _
         | Expr.MethodCall _
@@ -38,6 +40,17 @@ module Expr =
             Encode.object [ ("type", Encode.string "application")
                             ("func", jsonEncode func)
                             ("arg", jsonEncode arg) ]
+        | Expr.BuilderMethodCallChain (object, chain) ->
+            let chain =
+                chain
+                |> List.map (fun call ->
+                    Encode.object [ ("method", Encode.string call.Method)
+                                    ("args", encodeList call.Args) ])
+                |> Encode.list
+
+            Encode.object [ ("type", Encode.string "builderMethodCallChain")
+                            ("object", jsonEncode object)
+                            ("chain", chain) ]
         | Expr.FunctionCall (func, args) ->
             Encode.object [ ("type", Encode.string "functionCall")
                             ("func", Encode.string func)
@@ -81,6 +94,21 @@ module Expr =
             let func = toSrcWithParen func
             let arg = toSrcWithParen arg
             $"%s{func} %s{arg}"
+        | Expr.BuilderMethodCallChain (object, chain) ->
+            let object = toSrcWithParen object
+
+            let chain =
+                chain
+                |> List.map (fun call ->
+                    let args =
+                        call.Args
+                        |> List.map toSrcWithParen
+                        |> String.concat ", "
+
+                    $"|>> builder_at.%s{call.Method}(%s{args})")
+                |> String.concat " "
+
+            $"%s{object} %s{chain}"
         | Expr.FunctionCall (func, args) ->
             let args =
                 args
@@ -89,6 +117,7 @@ module Expr =
 
             $"%s{func} %s{args}"
         | Expr.Lambda (var, body) ->
+            let var = var.Replace("@", "_at")
             let body = toSrc body
             $"fun %s{var} -> %s{body}"
         | Expr.MethodCall (object, method, args) ->
@@ -163,9 +192,8 @@ module Expr =
 
                 Expr.FunctionCall(func, args)
         | Lambda (var, body) ->
-            let var = var.Name.Replace("@", "_at")
             let body = fromQuotationsExpr body
-            Expr.Lambda(var, body)
+            Expr.Lambda(var.Name, body)
         | NewAnonRecord fields ->
             let fields = fields |> Map.map (fun _ -> fromQuotationsExpr)
 
@@ -181,19 +209,54 @@ module Expr =
         | ValueWithName (_value, _ty, name) -> Expr.VarRef name
         | _ -> failwith $"Unsupported expression: %A{expr}"
 
+    let rec flatten (expr: Expr) : Expr =
+        match expr with
+        | Expr.BuilderMethodCallChain _
+        | Expr.StaticPropertyGet _
+        | Expr.StringLiteral _
+        | Expr.UnitLiteral
+        | Expr.VarRef _ -> expr
+        | Expr.Application (func, arg) -> Expr.Application(func = flatten func, arg = flatten arg)
+        | Expr.FunctionCall (func, args) -> Expr.FunctionCall(func, args |> List.map flatten)
+        | Expr.Lambda (var, body) -> Expr.Lambda(var, flatten body)
+        | Expr.NewAnonRecord fields ->
+            fields
+            |> Map.map (fun _ -> flatten)
+            |> Expr.NewAnonRecord
+        | Expr.NewUnionCase (name, fields) -> Expr.NewUnionCase(name, fields |> List.map flatten)
+        | Expr.MethodCall (Expr.VarRef "builder@", method, arg :: args) ->
+            match flatten arg with
+            | Expr.BuilderMethodCallChain (object, chain) ->
+                Expr.BuilderMethodCallChain(object, chain @ [ {| Method = method; Args = args |} ])
+            | arg ->
+                Expr.BuilderMethodCallChain(
+                    arg,
+                    [ {| Method = method
+                         Args = args |> List.map flatten |} ]
+                )
+        | Expr.MethodCall (object, method, args) ->
+            let object = flatten object
+            let args = args |> List.map flatten
+            Expr.MethodCall(object, method, args)
+
 open System.IO
 
 let writeJson (filename: string) (expr: Quotations.Expr) =
     let content =
         expr
         |> Expr.fromQuotationsExpr
+        |> Expr.flatten
         |> Expr.jsonEncode
         |> Encode.toString 2
 
     File.WriteAllText(filename, $"{content}\n")
 
 let writeSrc (filename: string) (expr: Quotations.Expr) =
-    let src = expr |> Expr.fromQuotationsExpr |> Expr.toSrc
+    let src =
+        expr
+        |> Expr.fromQuotationsExpr
+        |> Expr.flatten
+        |> Expr.toSrc
 
     let src =
         Fantomas.Core.CodeFormatter.FormatDocumentAsync(false, src)
