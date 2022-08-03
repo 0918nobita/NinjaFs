517f8b57e00e9d6250699562fe7b6dae9d68343a
Kodai Matsumoto <nobita.0918@gmail.com>
2022-08-03 13:16:12 +0900

    :sparkles: (docsGen) NinjaFs の各エンティティについて名前と URL を取得するようにした

--- docsGen/Program.fs
+++ docsGen/Program.fs
@@ -5,6 +5,15 @@ open FSharp.Formatting.ApiDocs
 let docModel =
     ApiDocs.GenerateModel([ ApiDocInput.FromFile("src/bin/Debug/netstandard2.0/NinjaFs.dll") ], "NinjaFs", [])
 
-docModel.Collection.Namespaces
-|> List.map (fun ns -> ns.Name)
-|> printfn "namespaces: %A"
+let collection = docModel.Collection
+
+collection.Namespaces
+|> List.iter (fun ns ->
+    printfn "%s" ns.Name
+
+    ns.Entities
+    |> List.iter (fun entity ->
+        printfn "  %s" entity.Name
+
+        printfn "    (%s)"
+        <| entity.Url("https://0918nobita.github.io/NinjaFs/", collection.CollectionName, true, ".fs")))
