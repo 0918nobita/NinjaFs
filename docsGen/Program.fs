0ff596e66158b0e1b11ce7ec5f084d2d70989fd0
Kodai Matsumoto <nobita.0918@gmail.com>
2022-08-03 13:02:59 +0900

    :new: (docsGen) ドキュメント生成用のプロジェクトを追加した

---
+++ docsGen/Program.fs
@@ -0,0 +1,10 @@
+﻿module NinjaFs.DocsGen
+
+open FSharp.Formatting.ApiDocs
+
+let docModel =
+    ApiDocs.GenerateModel([ ApiDocInput.FromFile("src/bin/Debug/netstandard2.0/NinjaFs.dll") ], "NinjaFs", [])
+
+docModel.Collection.Namespaces
+|> List.map (fun ns -> ns.Name)
+|> printfn "namespaces: %A"
